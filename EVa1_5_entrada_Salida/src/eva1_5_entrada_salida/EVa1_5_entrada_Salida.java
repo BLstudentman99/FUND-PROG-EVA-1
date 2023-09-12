b038097f1ffc4cd045648f871fdecdc8256e0709
BLstudentman99 <143562661+BLstudentman99@users.noreply.github.com>
2023-09-12 17:44:04 -0600

    Add files via upload

---
+++ EVa1_5_entrada_Salida/src/eva1_5_entrada_salida/EVa1_5_entrada_Salida.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva1_5_entrada_salida;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVa1_5_entrada_Salida {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
